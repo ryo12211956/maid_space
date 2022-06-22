c231c3f65f6557ce5116dff7e65fa8cd10a7071a
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-06-22 22:51:12 +0900

    Add files via upload

--- env.php
+++ env.php
@@ -1,4 +1,10 @@
 <?php
+
+define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
+define('DB_NAME', 'tennis');
+define('DB_USER', 'be31fcaddd81c0');
+define('DB_PASS', '3756fe50');
+
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
