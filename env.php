48aeac4e4cbb829d20b4f7415809d6a07abb9913
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-06-25 23:19:13 +0900

    Add files via upload

--- env.php
+++ env.php
@@ -2,6 +2,6 @@
  define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
  define('DB_NAME', 'heroku_cff8651284cd5d3');
  define('DB_USER', 'be31fcaddd81c0');
- define('DB_PASS', '3756fe50');
+ define('DB_PASS', '');
 ?>
 
