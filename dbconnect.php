14fa6d98aa6bf7c442f1a2fa1399624aed495a8c
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-06-26 17:01:28 +0900

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -11,7 +11,7 @@ function connect()
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
     try {
-        $pdo = new PDO($dsn, $user, $password, [
+        $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
