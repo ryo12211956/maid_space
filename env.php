8fe46a39f7b3edf557740158e78b2b15ddd2bd6b
sohopro <15377885+sohopro@users.noreply.github.com>
2022-07-25 08:23:36 +0900

    env変更

--- env.php
+++ env.php
@@ -1,8 +1,7 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-define('DB_HOST', 'ip-10-0-6-159.ec2.internal');
-define('DB_NAME', 'us-cdbr-east-05.cleardb.net');
-define('DB_USER', 'be31fcaddd81c0');
-define('DB_PASS', '3756fe50');
+define('DB_HOST', $url["host"]);
+define('DB_NAME', substr($url["path"], 1));
+define('DB_USER', $url["user"]);
+define('DB_PASS', $url["pass"]);
 ?>
